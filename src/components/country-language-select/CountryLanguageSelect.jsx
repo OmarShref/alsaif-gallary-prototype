bdc18b71d7bed721dbe59096e4b34ea5146c9ae6
Omar Sharif <mohandesomar@gmail.com>
2023-04-10 18:19:33 +0200

    ui fixes

--- src/components/country-language-select/CountryLanguageSelect.jsx
+++ src/components/country-language-select/CountryLanguageSelect.jsx
@@ -1,10 +1,15 @@
 import styles from "./CountryLanguageSelect.module.css";
+import { Show, createSignal } from "solid-js";
 import logo from "../../assets/logo.svg";
 import saFlag from "../../assets/Flag_of_Saudi_Arabia_(state).svg.png";
 import uaFlag from "../../assets/Flag_of_the_United_Arab_Emirates.svg.png";
 import kuFlag from "../../assets/Flag_of_Kuwait.svg.png";
 
 const CountryLanguageSelect = () => {
+  const [kuIsSelected, setKuIsSelected] = createSignal(false);
+  const [uaIsSelected, setUaIsSelected] = createSignal(false);
+  const [saIsSelected, setSaIsSelected] = createSignal(true);
+
   return (
     <>
       <div class={styles.container}>
@@ -18,24 +23,51 @@ const CountryLanguageSelect = () => {
             <div class={styles.line}></div>
           </div>
           <div class={styles.countries}>
-            <div class={styles.country}>
-              <i
-                class={"fa-solid fa-location-pin " + styles.selectedCountry}
-              ></i>
+            <div
+              class={styles.country}
+              onclick={() => {
+                setKuIsSelected(true);
+                setUaIsSelected(false);
+                setSaIsSelected(false);
+              }}
+            >
+              <Show when={kuIsSelected()}>
+                <i
+                  class={"fa-solid fa-location-pin " + styles.selectedCountry}
+                ></i>
+              </Show>
               <i class="fa-solid fa-location-pin"></i>
               <img src={kuFlag} alt="" />
             </div>
-            <div class={styles.country}>
-              {/* <i
-                class={"fa-solid fa-location-pin " + styles.selectedCountry}
-              ></i> */}
+            <div
+              class={styles.country}
+              onclick={() => {
+                setUaIsSelected(true);
+                setKuIsSelected(false);
+                setSaIsSelected(false);
+              }}
+            >
+              <Show when={uaIsSelected()}>
+                <i
+                  class={"fa-solid fa-location-pin " + styles.selectedCountry}
+                ></i>
+              </Show>
               <i class="fa-solid fa-location-pin"></i>
               <img src={uaFlag} alt="" />
             </div>
-            <div class={styles.country}>
-              {/* <i
-                class={"fa-solid fa-location-pin " + styles.selectedCountry}
-              ></i> */}
+            <div
+              class={styles.country}
+              onclick={() => {
+                setSaIsSelected(true);
+                setKuIsSelected(false);
+                setUaIsSelected(false);
+              }}
+            >
+              <Show when={saIsSelected()}>
+                <i
+                  class={"fa-solid fa-location-pin " + styles.selectedCountry}
+                ></i>
+              </Show>
               <i class="fa-solid fa-location-pin"></i>
               <img src={saFlag} alt="" />
             </div>
